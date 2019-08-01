35db4f7aa2091562af838a721a264f69da96b502
Chris Heppell <clheppell@icloud.com>
2019-08-01 17:49:01 +0100

    Search and favicon updates

--- search.php
+++ search.php
@@ -3,15 +3,34 @@
 
 <div class="container">
 
-  <div class="cell bg-primary-dark text-white mb-5">
-    <h1 class="mb-4">Search Results</h1>
-    <div id="search">
-      <?php get_search_form(); ?>
-    </div>
-  </div>
+  <div class="row justify-content-center">
+    <div class="col-lg-8">
 
-  <div class="row">
-    <div class="col-lg-8 blog-main">
+      <div class="cell bg-dark text-white mb-5">
+        <h1 class="mb-4">Search Results</h1>
+        <div id="search">
+        <?php
+
+          $search_terms = '';
+
+          if (isset($_GET["s"])) {
+            $search_terms = htmlspecialchars($_GET["s"]);
+          }
+
+          ?>
+
+          <form action="<?=get_home_url()?>/" id="searchform" method="get">
+            <label for="s" class="sr-only">Search</label>
+            <div class="input-group input-group-lg">
+              <input type="search" class="form-control" id="s" name="s" placeholder="Search the site"<?php if ( $search_terms !== '' ) { echo ' value="' . $search_terms . '"'; } ?> />
+              <div class="input-group-append">
+                <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i><span class="sr-only">Search</span></button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
       <div class="cell">
@@ -37,9 +56,6 @@
 
       <?php if ( function_exists('wp_bootstrap_pagination') ) wp_bootstrap_pagination(); ?>
 
-      </div><!-- /.blog-main -->
-    <div class="col-lg-4">
-      <?php get_sidebar(); ?>
     </div>
   </div>
 </div>
